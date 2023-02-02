2df04366d3bc3497e99bfe3ca03e572ec33f4185
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2023-02-02 08:27:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "../index.scss";
+function App() {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chit Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button type="submit">Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
